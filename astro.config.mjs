52cf35a371df6a6682fa6e37564dd0fd20b20853
MerchantPug <merchantpug@gmail.com>
2025-04-11 22:34:37 +1000

    Revert "Update dependencies and squash errors."
    This reverts commit 6836ca980d8b0c0eaf472281818cc73436da240d.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import tailwind from "@astrojs/tailwind";
+
 import icon from "astro-icon";
-
-import tailwindcss from "@tailwindcss/vite";
-
-import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()],
+  integrations: [tailwind(), icon()],
   output: "server",
-  vite: {
-    plugins: [tailwindcss()],
-  },
-  adapter: vercel(),
 });
