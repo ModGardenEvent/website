27a3316b8be8419eaa293aced44ba336c09a2d8d
Chrys Chrysanthemum <cow@chrysanthemum.lgbt>
2026-07-01 15:31:24 +1000

    feat: Update website to latest packages.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import icon from "astro-icon";
-
-import tailwindcss from "@tailwindcss/vite";
-
 import node from "@astrojs/node";
-
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon()],
   output: "server",
-
-  vite: {
-    plugins:
-    [
-      // @ts-ignore
-      tailwindcss()
-    ],
-  },
 
   adapter: node({
     mode: "standalone"
